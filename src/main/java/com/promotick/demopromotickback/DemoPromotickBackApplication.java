43ff039fef79ecd515ad9971a3a93ead79e3af91
Christian Misaico <christianmisaico@gmail.com>
2023-12-13 10:40:23 -0500

    feat: New proyect demo-promotick

---
+++ src/main/java/com/promotick/demopromotickback/DemoPromotickBackApplication.java
@@ -0,0 +1,13 @@
+package com.promotick.demopromotickback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoPromotickBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoPromotickBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/promotick/demopromotickback/DemoPromotickBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.promotick.demopromotickback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoPromotickBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
